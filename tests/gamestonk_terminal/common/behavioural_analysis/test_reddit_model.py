e13084f12238734a0745d89eede1f178cf8771c5
jbushago <jbushago@purdue.edu>
2022-03-03 13:38:44 +0000

    Reformat reddit model and view.

--- tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_model.py
+++ tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_model.py
@@ -1,22 +1,29 @@
 import pytest
 
+
 def test_get_watchlists():
     pass
+
 
 def test_get_popular_tickers():
     pass
 
+
 def test_get_spac_community():
     pass
+
 
 def test_get_spac():
     pass
 
+
 def test_get_wsb_community():
     pass
 
+
 def test_get_due_dilligence():
     pass
+
 
 def test_get_posts_about():
     pass
