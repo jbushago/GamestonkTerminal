63fb003535ce3758a285209bc29aa631bfe5c8bc
montoys <montoys@purdue.edu>
2022-03-02 20:23:44 -0500

    refactored code to follow black code style

--- tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_helpers.py
+++ tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_helpers.py
@@ -1,16 +1,21 @@
-#IMPORTANT INTERNAL
+# IMPORTANT INTERNAL
 import pytest
 from gamestonk_terminal.common.behavioural_analysis import reddit_helpers
+
 
 @pytest.mark.parametrize(
     "mocked_input , expected",
     [
-        (["MSFT, BABA, ECE49595 is the best, lets go! Diamond hands all the way. DOGE to the moon."], ["MSFT", "BABA", "ECE", "DOGE"]),
+        (
+            [
+                "$MSFT, $BABA, ECE49595 is the best, lets go! Diamond hands all the way. DOGE to the moon."
+            ],
+            ["MSFT", "BABA", "ECE", "DOGE"],
+        ),
         ("", []),
-        ("Arsenal is the best football club in the world", [])
+        ("Arsenal is the best football club in the world", []),
     ],
 )
-
 def test_find_tickers(mocked_input, expected):
     generated_tickers = reddit_helpers.find_all_body_tickers(mocked_input)
     assert set(generated_tickers) == set(expected)
