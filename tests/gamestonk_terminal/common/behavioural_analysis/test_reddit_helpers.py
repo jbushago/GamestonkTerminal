4b4f2e42ef8d8c5dbaea14a9f952395e5914c7f9
montoys <montoys@purdue.edu>
2022-03-02 20:04:22 -0500

    added test for find_tickersfunction

--- tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_helpers.py
+++ tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_helpers.py
@@ -1,7 +1,16 @@
 #IMPORTANT INTERNAL
+import pytest
 from gamestonk_terminal.common.behavioural_analysis import reddit_helpers
 
-def test_find_tickers(expected, mocker):
-    mocker.patch(
-        target="gamestonk_terminal.common.behavioural_analysis.reddit_helpers"
-    )
+@pytest.mark.parametrize(
+    "mocked_input , expected",
+    [
+        (["MSFT, BABA, ECE49595 is the best, lets go! Diamond hands all the way. DOGE to the moon."], ["MSFT", "BABA", "ECE", "DOGE"]),
+        ("", []),
+        ("Arsenal is the best football club in the world", [])
+    ],
+)
+
+def test_find_tickers(mocked_input, expected):
+    generated_tickers = reddit_helpers.find_all_body_tickers(mocked_input)
+    assert set(generated_tickers) == set(expected)
