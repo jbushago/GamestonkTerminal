65d8881581435da9ecd8bfe018c62731c42a334c
montoys <montoys@purdue.edu>
2022-03-31 16:21:04 -0400

    black code style formatting

--- tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_model.py
+++ tests/gamestonk_terminal/common/behavioural_analysis/test_reddit_model.py
@@ -3,19 +3,20 @@ import pytest
 from gamestonk_terminal.common.behavioural_analysis import reddit_model
 from unittest.mock import patch, Mock
 
-    # @pytest.mark.parametrize(
-    #     "get_posts_about",
-    #     [
-    #         #Tuple with input and expected
-    #         ("0", tuple([[], dict, 0])),
-    #     ],
-    # )
+# @pytest.mark.parametrize(
+#     "get_posts_about",
+#     [
+#         #Tuple with input and expected
+#         ("0", tuple([[], dict, 0])),
+#     ],
+# )
+
 
 @patch("praw.Reddit")
 def test_get_posts_about(reddit_mock):
     api_mock = Mock()
     reddit_mock.return_value = api_mock
-    api_mock.subreddit = Mock() 
+    api_mock.subreddit = Mock()
     subreddit_mock = Mock()
     api_mock.subreddit.return_value = subreddit_mock
     mock_function = Mock()
